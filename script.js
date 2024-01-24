fd43c61632babe4a783f5d0d93959dbee9b37034
qgerman2 <qgerman2@gmail.com>
2024-01-24 18:52:39 -0300

    puede ser pa

--- script.js
+++ script.js
@@ -1,52 +1,70 @@
 addEventListener("load", () => {
     var main = d3.select("main");
-    var scrolly = main.select("#scrolly");
-    var figure = scrolly.select("figure");
-    var article = scrolly.select("article");
-    var step = article.selectAll(".step");
+    var scrolly1 = main.selectAll("#scrolly-overlay");
+    var figure1 = scrolly1.select("figure");
+    var article1 = scrolly1.select("article");
+    var step1 = article1.selectAll(".step");
 
-    var scroller = scrollama();
+    var scrolly2 = main.selectAll("#scrolly-side");
+    var figure2 = scrolly2.select("figure");
+    var article2 = scrolly2.select("article");
+    var step2 = article2.selectAll(".step");
 
-    // generic window resize listener event
+    var scroller1 = scrollama();
+    var scroller2 = scrollama();
+
     function handleResize() {
-        console.log("reisze")
-        // 1. update height of step elements
         var stepH = Math.floor(window.innerHeight * 0.75);
-        step.style("height", stepH + "px");
+        step1.style("height", stepH + "px");
+        step2.style("height", stepH + "px");
 
         var figureHeight = window.innerHeight / 2;
         var figureMarginTop = (window.innerHeight - figureHeight) / 2;
 
-        figure
+        figure1
+            .style("height", figureHeight + "px")
+            .style("top", figureMarginTop + "px");
+        figure2
             .style("height", figureHeight + "px")
             .style("top", figureMarginTop + "px");
 
-        // 3. tell scrollama to update new element dimensions
-        scroller.resize();
+        scroller1.resize();
+        scroller2.resize();
     }
 
-    // scrollama event handlers
-    function handleStepEnter(response) {
+    function handleStepEnter1(response) {
         console.log(response);
-
-        step.classed("is-active", function (d, i) {
+        step1.classed("is-active", function (d, i) {
             return i === response.index;
         });
-
-        figure.select("p").text(response.index + "miau");
+        figure1.select("p").text(response.index + 1);
     }
+
+    function handleStepEnter2(response) {
+        console.log(response);
+        step2.classed("is-active", function (d, i) {
+            return i === response.index;
+        });
+        figure2.select("p").text(response.index + 1);
+    }
+
 
     function init() {
         handleResize();
-        scroller
+        scroller1
             .setup({
-                step: "#scrolly article .step",
+                step: "#scrolly-overlay article .step",
                 offset: 0.33,
                 debug: false
             })
-            .onStepEnter(handleStepEnter);
+            .onStepEnter(handleStepEnter1);
+        scroller2
+            .setup({
+                step: "#scrolly-side article .step",
+                offset: 0.33,
+                debug: false
+            })
+            .onStepEnter(handleStepEnter2);
     }
-
-    window.addEventListener("resize", handleResize);
     init();
 })
